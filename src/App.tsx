b1e77407c09660d2c844aaf8ed8018cb2741a7b2
Povilas <povilaskazlauskas2003@gmail.com>
2023-12-08 10:20:22 +0100

    wallpaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import './index.css';
 
 function App() {
+
   return (
-    <div className='text-3xl'>Hello</div>
+    
+    <div className='bg-image bg-cover h-screen flex items-center justify-center'>
+      <button className='border-4 border-white text-white h-16 w-96'>Search...</button>
+    </div>
   );
 }
 
